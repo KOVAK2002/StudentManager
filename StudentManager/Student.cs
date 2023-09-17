681ba2ce37cc5b4108f7b970a9419be3bc5f43be
Borna Kovačević <bkovacevic@vub.hr>
2023-09-17 23:23:39 +0200

    Drastične promjene u Student.cs uz veliku pomoc koristeni su delegati i eventi

--- StudentManager/Student.cs
+++ StudentManager/Student.cs
@@ -1,23 +1,27 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Data;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Xml.Linq;
+using System.IO;
 using MySql.Data.MySqlClient;
 
 namespace StudentManager
 {
     internal class Student
     {
+        // Definiranje dlegata
+        public delegate void StudentOperationEventHandler(object sender, StudentEventArgs e);
+
+        // Delegati za metode
+        public event StudentOperationEventHandler StudentInserted;
+        public event StudentOperationEventHandler StudentUpdated;
+        public event StudentOperationEventHandler StudentDeleted;
+
         MY_DB db = new MY_DB();
+
         public bool InsertStudent(string fname, string lname, DateTime bdate, string phone, string gender, string address, MemoryStream picture)
         {
             MySqlCommand command = new MySqlCommand("INSERT INTO `students`(`FirstName`, `LastName`, `BirthDate`, `Gender`, `Phone`, `Address`, `Image`) VALUES (@fn,@ln,@bd,@gdr,@phn,@addr,@img)", db.GetConnection);
-            
-            //@fn,@ln,@bd,@gdr,@phn,@addr,@img
-            command.Parameters.Add("@fn",MySqlDbType.VarChar).Value = fname;
+
+            command.Parameters.Add("@fn", MySqlDbType.VarChar).Value = fname;
             command.Parameters.Add("@ln", MySqlDbType.VarChar).Value = lname;
             command.Parameters.Add("@bd", MySqlDbType.Date).Value = bdate;
             command.Parameters.Add("@phn", MySqlDbType.VarChar).Value = phone;
@@ -25,24 +29,22 @@ namespace StudentManager
             command.Parameters.Add("@gdr", MySqlDbType.VarChar).Value = gender;
             command.Parameters.Add("@img", MySqlDbType.Blob).Value = picture.ToArray();
 
-
             db.openConnection();
-                if(command.ExecuteNonQuery()==1) {
+
+            if (command.ExecuteNonQuery() == 1)
+            {
+                // provjera inserta
+                OnStudentInserted(new StudentEventArgs(fname, lname, bdate, phone, gender, address, picture));
                 db.closeConnection();
                 return true;
-
             }
             else
             {
                 db.closeConnection();
                 return false;
             }
-
-            return true;
         }
 
-
-        //stvoriti funkciju za dodavanje studenta u refresh listu
         public DataTable getStudents(MySqlCommand command)
         {
             command.Connection = db.GetConnection;
@@ -50,14 +52,12 @@ namespace StudentManager
             DataTable table = new DataTable();
             adapter.Fill(table);
             return table;
-
-
         }
-        public bool updateStudent(int id,string fname, string lname, DateTime bdate, string phone, string gender, string address, MemoryStream picture)
-        {
-            MySqlCommand command = new MySqlCommand("UPDATE `students` SET `FirstName`=@fn,`LastName`=@ln,`BirthDate`=@bd,`Gender`=@gnd,`Phone`=@phn,`Address`=@addr,`Image`=@img WHERE `Id`=@ID", db.GetConnection);
 
-            //@fn,@ln,@bd,@gdr,@phn,@addr,@img
+        public bool updateStudent(int id, string fname, string lname, DateTime bdate, string phone, string gender, string address, MemoryStream picture)
+        {
+            MySqlCommand command = new MySqlCommand("UPDATE `students` SET `FirstName`=@fn,`LastName`=@ln,`BirthDate`=@bd,`Gender`=@gdr,`Phone`=@phn,`Address`=@addr,`Image`=@img WHERE `Id`=@ID", db.GetConnection);
+
             command.Parameters.Add("@ID", MySqlDbType.Int32).Value = id;
             command.Parameters.Add("@fn", MySqlDbType.VarChar).Value = fname;
             command.Parameters.Add("@ln", MySqlDbType.VarChar).Value = lname;
@@ -67,43 +67,79 @@ namespace StudentManager
             command.Parameters.Add("@gdr", MySqlDbType.VarChar).Value = gender;
             command.Parameters.Add("@img", MySqlDbType.Blob).Value = picture.ToArray();
 
-
             db.openConnection();
+
             if (command.ExecuteNonQuery() == 1)
             {
+                // provjera update
+                OnStudentUpdated(new StudentEventArgs(fname, lname, bdate, phone, gender, address, picture));
                 db.closeConnection();
                 return true;
-
             }
             else
             {
                 db.closeConnection();
                 return false;
             }
-
         }
+
         public bool deleteStudent(int id)
         {
             MySqlCommand command = new MySqlCommand("DELETE FROM `students` WHERE `Id`=@studentId", db.GetConnection);
 
-            //@fn,@ln,@bd,@gdr,@phn,@addr,@img
             command.Parameters.Add("@studentID", MySqlDbType.Int32).Value = id;
-         
+
             db.openConnection();
+
             if (command.ExecuteNonQuery() == 1)
             {
+                // provjera update
+                OnStudentDeleted(new StudentEventArgs(null, null, DateTime.MinValue, null, null, null, null));
                 db.closeConnection();
                 return true;
-
             }
             else
             {
                 db.closeConnection();
                 return false;
             }
-
-
         }
 
+        protected virtual void OnStudentInserted(StudentEventArgs e)
+        {
+            StudentInserted?.Invoke(this, e);
+        }
+
+        protected virtual void OnStudentUpdated(StudentEventArgs e)
+        {
+            StudentUpdated?.Invoke(this, e);
+        }
+
+        protected virtual void OnStudentDeleted(StudentEventArgs e)
+        {
+            StudentDeleted?.Invoke(this, e);
+        }
+    }
+
+    public class StudentEventArgs : EventArgs
+    {
+        public string FirstName { get; }
+        public string LastName { get; }
+        public DateTime BirthDate { get; }
+        public string Phone { get; }
+        public string Gender { get; }
+        public string Address { get; }
+        public MemoryStream Picture { get; }
+
+        public StudentEventArgs(string fname, string lname, DateTime bdate, string phone, string gender, string address, MemoryStream picture)
+        {
+            FirstName = fname;
+            LastName = lname;
+            BirthDate = bdate;
+            Phone = phone;
+            Gender = gender;
+            Address = address;
+            Picture = picture;
+        }
     }
 }
--- StudentManager/UpdateDeleteStudentForm.Designer.cs
+++ StudentManager/UpdateDeleteStudentForm.Designer.cs
@@ -34,25 +34,37 @@
             lbBirth = new Label();
             lbLast_Name = new Label();
             lbName = new Label();
+            pictureBox1 = new PictureBox();
+            rbFemale = new RadioButton();
+            rbMale = new RadioButton();
+            textBox4 = new TextBox();
+            textBox3 = new TextBox();
+            lbPicture = new Label();
+            lbAddr = new Label();
+            lbPhone = new Label();
+            lbGender = new Label();
+            tbID = new TextBox();
+            lbid = new Label();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
             // 
             // dateTimePicker1
             // 
-            dateTimePicker1.Location = new Point(344, 215);
+            dateTimePicker1.Location = new Point(196, 205);
             dateTimePicker1.Name = "dateTimePicker1";
             dateTimePicker1.Size = new Size(250, 27);
             dateTimePicker1.TabIndex = 17;
             // 
             // textBox2
             // 
-            textBox2.Location = new Point(345, 159);
+            textBox2.Location = new Point(197, 149);
             textBox2.Name = "textBox2";
             textBox2.Size = new Size(249, 27);
             textBox2.TabIndex = 16;
             // 
             // textBox1
             // 
-            textBox1.Location = new Point(345, 108);
+            textBox1.Location = new Point(197, 98);
             textBox1.Name = "textBox1";
             textBox1.Size = new Size(249, 27);
             textBox1.TabIndex = 15;
@@ -61,7 +73,7 @@
             // 
             lbBirth.AutoSize = true;
             lbBirth.Font = new Font("Segoe UI", 15F, FontStyle.Regular, GraphicsUnit.Point);
-            lbBirth.Location = new Point(229, 203);
+            lbBirth.Location = new Point(81, 193);
             lbBirth.Name = "lbBirth";
             lbBirth.Size = new Size(78, 35);
             lbBirth.TabIndex = 14;
@@ -71,7 +83,7 @@
             // 
             lbLast_Name.AutoSize = true;
             lbLast_Name.Font = new Font("Segoe UI", 15F, FontStyle.Regular, GraphicsUnit.Point);
-            lbLast_Name.Location = new Point(162, 151);
+            lbLast_Name.Location = new Point(14, 141);
             lbLast_Name.Name = "lbLast_Name";
             lbLast_Name.Size = new Size(145, 35);
             lbLast_Name.TabIndex = 13;
@@ -81,17 +93,133 @@
             // 
             lbName.AutoSize = true;
             lbName.Font = new Font("Segoe UI", 15F, FontStyle.Regular, GraphicsUnit.Point);
-            lbName.Location = new Point(160, 100);
+            lbName.Location = new Point(12, 90);
             lbName.Name = "lbName";
             lbName.Size = new Size(147, 35);
             lbName.TabIndex = 12;
             lbName.Text = "First Name :";
             // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(196, 334);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(249, 200);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 26;
+            pictureBox1.TabStop = false;
+            // 
+            // rbFemale
+            // 
+            rbFemale.AutoSize = true;
+            rbFemale.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            rbFemale.Location = new Point(835, 50);
+            rbFemale.Name = "rbFemale";
+            rbFemale.Size = new Size(95, 32);
+            rbFemale.TabIndex = 25;
+            rbFemale.TabStop = true;
+            rbFemale.Text = "Female";
+            rbFemale.UseVisualStyleBackColor = true;
+            // 
+            // rbMale
+            // 
+            rbMale.AutoSize = true;
+            rbMale.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            rbMale.Location = new Point(681, 50);
+            rbMale.Name = "rbMale";
+            rbMale.Size = new Size(76, 32);
+            rbMale.TabIndex = 24;
+            rbMale.TabStop = true;
+            rbMale.Text = "Male";
+            rbMale.UseVisualStyleBackColor = true;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(681, 156);
+            textBox4.Multiline = true;
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(249, 92);
+            textBox4.TabIndex = 23;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(681, 115);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(249, 27);
+            textBox3.TabIndex = 22;
+            // 
+            // lbPicture
+            // 
+            lbPicture.AutoSize = true;
+            lbPicture.Font = new Font("Segoe UI", 15F, FontStyle.Regular, GraphicsUnit.Point);
+            lbPicture.Location = new Point(68, 293);
+            lbPicture.Name = "lbPicture";
+            lbPicture.Size = new Size(91, 35);
+            lbPicture.TabIndex = 21;
+            lbPicture.Text = "Picture";
+            // 
+            // lbAddr
+            // 
+            lbAddr.AutoSize = true;
+            lbAddr.Font = new Font("Segoe UI", 15F, FontStyle.Regular, GraphicsUnit.Point);
+            lbAddr.Location = new Point(547, 156);
+            lbAddr.Name = "lbAddr";
+            lbAddr.Size = new Size(117, 35);
+            lbAddr.TabIndex = 20;
+            lbAddr.Text = "Address :";
+            // 
+            // lbPhone
+            // 
+            lbPhone.AutoSize = true;
+            lbPhone.Font = new Font("Segoe UI", 15F, FontStyle.Regular, GraphicsUnit.Point);
+            lbPhone.Location = new Point(547, 115);
+            lbPhone.Name = "lbPhone";
+            lbPhone.Size = new Size(97, 35);
+            lbPhone.TabIndex = 19;
+            lbPhone.Text = "Phone :";
+            // 
+            // lbGender
+            // 
+            lbGender.AutoSize = true;
+            lbGender.Font = new Font("Segoe UI", 15F, FontStyle.Regular, GraphicsUnit.Point);
+            lbGender.Location = new Point(547, 47);
+            lbGender.Name = "lbGender";
+            lbGender.Size = new Size(108, 35);
+            lbGender.TabIndex = 18;
+            lbGender.Text = "Gender :";
+            // 
+            // tbID
+            // 
+            tbID.Location = new Point(197, 46);
+            tbID.Name = "tbID";
+            tbID.Size = new Size(249, 27);
+            tbID.TabIndex = 28;
+            // 
+            // lbid
+            // 
+            lbid.AutoSize = true;
+            lbid.Font = new Font("Segoe UI", 15F, FontStyle.Regular, GraphicsUnit.Point);
+            lbid.Location = new Point(107, 38);
+            lbid.Name = "lbid";
+            lbid.Size = new Size(52, 35);
+            lbid.TabIndex = 27;
+            lbid.Text = "ID :";
+            // 
             // UpdateDeleteStudentForm
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(1032, 571);
+            Controls.Add(tbID);
+            Controls.Add(lbid);
+            Controls.Add(pictureBox1);
+            Controls.Add(rbFemale);
+            Controls.Add(rbMale);
+            Controls.Add(textBox4);
+            Controls.Add(textBox3);
+            Controls.Add(lbPicture);
+            Controls.Add(lbAddr);
+            Controls.Add(lbPhone);
+            Controls.Add(lbGender);
             Controls.Add(dateTimePicker1);
             Controls.Add(textBox2);
             Controls.Add(textBox1);
@@ -101,6 +229,7 @@
             Name = "UpdateDeleteStudentForm";
             Text = "UpdateDeleteStudentForm";
             Load += UpdateDeleteStudentForm_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -113,5 +242,16 @@
         private Label lbBirth;
         private Label lbLast_Name;
         private Label lbName;
+        private PictureBox pictureBox1;
+        private RadioButton rbFemale;
+        private RadioButton rbMale;
+        private TextBox textBox4;
+        private TextBox textBox3;
+        private Label lbPicture;
+        private Label lbAddr;
+        private Label lbPhone;
+        private Label lbGender;
+        private TextBox tbID;
+        private Label lbid;
     }
 }
